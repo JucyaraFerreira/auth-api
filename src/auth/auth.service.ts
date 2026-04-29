6de26fd18d221fb4bd4263a5a5054a5153e7e90f
jucyara ferreira <jucyaraferreira57@gmail.com>
2026-04-29 13:00:08 -0300

    API utilizando nestjs, prisma e docker

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,117 @@
+import {
+  Injectable,
+  BadRequestException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { randomBytes } from 'crypto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async register(data: any) {
+    const userExists = await this.prisma.user.findUnique({
+      where: { email: data.email },
+    });
+
+    if (userExists) {
+      throw new BadRequestException('Esse E-mail já existe!');
+    }
+
+    const hash = await bcrypt.hash(data.password, 10);
+
+    const user = await this.prisma.user.create({
+      data: {
+        name: data.name,
+        email: data.email,
+        password: hash,
+      },
+    });
+
+    return user;
+  }
+
+  async login(data: any) {
+    const user = await this.prisma.user.findUnique({
+      where: { email: data.email },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Credenciais inválidas!');
+    }
+
+    const match = await bcrypt.compare(data.password, user.password);
+
+    if (!match) {
+      throw new UnauthorizedException('Credenciais inválidas!');
+    }
+
+    const token = await this.jwtService.signAsync({
+      sub: user.id,
+      email: user.email,
+    });
+
+    return {
+      access_token: token,
+    };
+  }
+
+  async forgotPassword(data: any) {
+    const user = await this.prisma.user.findUnique({
+      where: { email: data.email },
+    });
+
+    if (!user) {
+      return { message: 'Se existir, enviamos um token' };
+    }
+
+    const token = randomBytes(32).toString('hex');
+
+    const exp = new Date();
+    exp.setMinutes(exp.getMinutes() + 15);
+
+    await this.prisma.user.update({
+      where: { email: data.email },
+      data: {
+        resetPasswordToken: token,
+        resetPasswordExp: exp,
+      },
+    });
+
+    return { token };
+  }
+
+  async resetPassword(data: any) {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        resetPasswordToken: data.token,
+        resetPasswordExp: {
+          gt: new Date(),
+        },
+      },
+    });
+
+    if (!user) {
+      throw new BadRequestException('Token inválido!');
+    }
+
+    const hash = await bcrypt.hash(data.newPassword, 10);
+
+    await this.prisma.user.update({
+      where: { id: user.id },
+      data: {
+        password: hash,
+        resetPasswordToken: null,
+        resetPasswordExp: null,
+      },
+    });
+
+    return { message: 'Senha atualizada com sucesso' };
+  }
+}
